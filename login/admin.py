ee14642a2527ceb0177732a58cd71b6969a73c1e
dbd62 <dbd62@cornell.edu>
2017-03-30 01:05:28 -0400

    editing

--- login/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from login.models import *
-
-# Register your models here.
-
-admin.site.register(UserProfile)
-admin.site.register(FeeType)
-admin.site.register(Payment)
